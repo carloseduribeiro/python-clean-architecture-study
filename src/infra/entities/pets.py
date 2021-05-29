6dfd209d2714f7f807f66b8b3d11b549b8002944
carloseduribeiro <carloseribeiro96@gmail.com>
2021-05-29 15:16:22 -0300

    feat: implementing infra with config, entities, repo

---
+++ src/infra/entities/pets.py
@@ -0,0 +1,28 @@
+import enum
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.sql.sqltypes import Enum
+from src.infra.config import Base
+
+
+class AnimalTypes(enum.Enum):
+    """Defininf Animals Types"""
+
+    dog = "dog"
+    cat = "cat"
+    fish = "fish"
+    turtle = "turtle"
+
+
+class Pets(Base):
+    """Pets Entity"""
+
+    __tablename__ = "pets"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    specie = Column(Enum(AnimalTypes), nullable=False)
+    age = Column(Integer)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    def __rep__(self):
+        return f"Pet: [name = {self.name}, specie = {self.specie}, user_id = {self.user_id}]"
