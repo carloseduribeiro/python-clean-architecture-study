39103c4843535b2c9c580691775c682ad2030ea2
carloseduribeiro <carloseribeiro96@gmail.com>
2021-06-01 00:37:09 -0300

    feat: Implementing select_pet in PetRepository

--- src/infra/repo/pet_repository.py
+++ src/infra/repo/pet_repository.py
@@ -1,5 +1,6 @@
 # pylint: disable=E1101
 
+from typing import List
 from src.domain.models.pets import Pets
 from src.infra.config.db_config import DBConnectionHandler
 from src.infra.entities.pets import Pets as PetsModel
@@ -36,5 +37,56 @@ class PetRepository:
                 raise
             finally:
                 db_connection.session.close()
+
+        return None
+
+    @classmethod
+    def select_pet(cls, pet_id: int = None, user_id: int = None) -> List[Pets]:
+        """Select data in pet entity by id and/or user id.
+        :param  - pet_id:    Id of the pet registry.
+                - user_id:   Id of the owner.
+        :return - List with the pets selected.
+        """
+
+        try:
+            query_data = None
+
+            if pet_id and not user_id:
+
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(PetsModel)
+                        .filter_by(id=pet_id)
+                        .one()
+                    )
+                query_data = [data]
+
+            elif user_id and not pet_id:
+
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(PetsModel)
+                        .filter_by(user_id=user_id)
+                        .all()
+                    )
+                    query_data = data
+
+            elif pet_id and user_id:
+
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(PetsModel)
+                        .filter_by(id=pet_id, user_id=user_id)
+                        .one()
+                    )
+                    query_data = [data]
+
+            return query_data
+
+        except:
+            db_connection.session.rollback()
+            raise
+        finally:
+            db_connection.session.close()
 
         return None
--- src/infra/repo/pet_repository_test.py
+++ src/infra/repo/pet_repository_test.py
@@ -1,5 +1,7 @@
 from faker import Faker
+from src.infra.entities import Pets
 from src.infra.config import DBConnectionHandler
+from src.infra.entities.pets import AnimalTypes
 from .pet_repository import PetRepository
 
 faker = Faker()
@@ -29,3 +31,34 @@ def test_insert_pet():
     assert new_pet.specie == query_pet.specie
     assert new_pet.age == query_pet.age
     assert new_pet.user_id == query_pet.user_id
+
+
+def test_seelct_pet():
+    """Should select a pet in Pets entity and compare it"""
+
+    pet_id = faker.random_number(digits=4)
+    name = faker.name()
+    specie = "fish"
+    age = faker.random_number(digits=1)
+    user_id = faker.random_number()
+
+    specie_mock = AnimalTypes(specie)
+    data = Pets(id=pet_id, name=name, specie=specie_mock, age=age, user_id=user_id)
+
+    # SQL Commands:
+    engine = db_conenction_handler.get_engine()
+    engine.execute(
+        "INSERT INTO pets(id, name, specie, age, user_id) VALUES ('{}','{}','{}','{}','{}');".format(
+            pet_id, name, specie, age, user_id
+        )
+    )
+
+    query_pets1 = pet_repository.select_pet(pet_id=pet_id)
+    query_pets2 = pet_repository.select_pet(user_id=user_id)
+    query_pets3 = pet_repository.select_pet(pet_id=pet_id, user_id=user_id)
+
+    assert data in query_pets1
+    assert data in query_pets2
+    assert data in query_pets3
+
+    engine.execute("DELETE FROM pets WHERE id='{}';".format(pet_id))
